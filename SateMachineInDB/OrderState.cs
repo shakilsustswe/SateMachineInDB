2d1adb709d5ac1b55e8b07b1b6edd2ab2d01f81e
Ashiqul Hasan Shakil <ashiqul.shakil@orbitax.com>
2025-09-18 13:26:25 +0600

    Added OrderState

---
+++ SateMachineInDB/OrderState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace SateMachineInDB
+{
+    public class OrderState:SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        public string CurrentState { get; set; } = "";
+        public bool RefundDone { get; set; }
+    }
+}
